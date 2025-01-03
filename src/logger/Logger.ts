306eb54b479db309f157bde55f03f8b28593ac5b
johnmuthua <caveomuthama@gmail.com>
2025-01-03 11:53:28 +0300

    feat: add logger

---
+++ src/logger/Logger.ts
@@ -0,0 +1,30 @@
+import pino from 'pino'
+
+const transport = pino.transport({
+  targets: [
+    {
+      target: 'pino-pretty',
+      options: {
+        destination: './logs/output.log',
+        mkdir: true,
+        colorize: false
+      }
+    },
+    {
+      target: 'pino-pretty',
+      options: {
+        destination: process.stdout.fd,
+        colorize: true
+      }
+    }
+  ]
+})
+
+const logger = pino(
+  {
+    level: 'info'
+  },
+  transport
+)
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { readFileSync } from 'node:fs'
 import resolvers from './graphql/resolvers'
+import logger from './logger/Logger'
 
 const typeDefs = readFileSync('./src/graphql/schema.graphql', 'utf8')
 const server = new ApolloServer({
@@ -13,7 +14,7 @@ const startServer = async () => {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
   })
-  console.log(`Server ready at ${url}`)
+  logger.info(`Server ready at ${url}`)
 }
 
 startServer()
