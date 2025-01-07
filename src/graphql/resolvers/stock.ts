bc5099d1f387ff0d149f1a5f8bed7d249072a0e6
johnmuthua <caveomuthama@gmail.com>
2025-01-07 22:20:52 +0300

    chore: eslint errors

--- src/graphql/resolvers/stock.ts
+++ src/graphql/resolvers/stock.ts
@@ -1,9 +1,21 @@
+import { getDailyData } from '../../utils/helpers'
+import logger from '../../logger/Logger'
+
 const StockResolvers = {
   Query: {
-    getStockData: () => ({
-      symbol: 'AAPL',
-      price: 120.0,
-    }),
+    getStockData: async (_parent: any, { symbol }) => {
+      try {
+        const data: any = await getDailyData(symbol)
+        if (data['Error Message']) {
+          logger.error(data['Error Message'])
+          return { success: false, error: data['Error Message'] }
+        }
+        return { success: true, data: data['Time Series (Daily)'] }
+      } catch (error) {
+        logger.error(error)
+        return { success: false, error }
+      }
+    },
   },
 }
 
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,24 +1,29 @@
 import request from 'request'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const apiKey = process.env.ALPHA_VANTAGE_API_KEY
 export const getDailyData = async (symbol: string) => {
-  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=demo`
-  const response = await request.get(
-    {
-      url,
-      json: true,
-      headers: { 'User-Agent': 'request' },
-    },
-    (err, res, data) => {
-      if (err) {
-        return { err }
+  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`
+  return new Promise((resolve, reject) => {
+    request.get(
+      {
+        url,
+        json: true,
+        headers: { 'User-Agent': 'request' },
+      },
+      (err, res, data) => {
+        if (err) {
+          reject(err)
+        } else if (res.statusCode !== 200) {
+          reject(new Error(`Status Code: ${res.statusCode}`))
+        } else {
+          resolve(data)
+        }
       }
-      if (res.statusCode !== 200) {
-        return { Status: res.statusCode }
-      }
-      return { data }
-    }
-  )
-  return response
+    )
+  })
 }
 
 export const getSingleDailyData = async (symbol: string) => {
